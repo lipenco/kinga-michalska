d4378a0eaa8312477a4c3f5f265387e9ee3f79c4
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-21 12:11:15 +0400

    test

--- js/impress-layout.js
+++ js/impress-layout.js
@@ -1,22 +1,39 @@
-
-function layoutCircleV2(options) {
-    var radius = options.radius;
+function layoutSnakeGrid(options) {
+    var numberOfColumns = options.numberOfColumns;
+    var distanceX = options.distanceX;
+    var distanceY = options.distanceY;
     var elements = document.querySelectorAll(".step");
-    if (!radius || radius < 900) {
-        radius = 1024 / (2 * Math.tan(Math.PI / elements.length));
+    var positionX = 100;
+    var positionY = 100;
+    var rowType = "even";
+    if (!distanceX || distanceX < 900) {
+        distanceX = 1500;
     }
-    var r = radius;
-    for (var i = 0; i < elements.length; i++) {
-        var phi = -i / (elements.length - 1) * 2 * Math.PI;
-        var x = r * Math.cos(phi);
-        var y = r * Math.sin(phi);
-        var rotation = phi / (2 * Math.PI) * 360 - 90;
-        elements[i].dataset.x = Math.round(x);
-        elements[i].dataset.y = Math.round(y);
-        elements[i].dataset.rotateY = Math.round(rotation) - 180;
-        elements[i].dataset.rotateX = 90;
+    if (!distanceY || distanceY < 700) {
+        distanceY = 1000;
+    }
+    if (!numberOfColumns || numberOfColumns < 1) {
+        numberOfColumns = 5;
+    }
+    for (var index = 0; index < elements.length; index++) {
+        elements[index].dataset.x = positionX;
+        elements[index].dataset.y = positionY;
+        if ((index + 1) % numberOfColumns === 0) {
+            positionY = positionY + distanceY;
+            if (rowType === "even") {
+                rowType = "odd";
+                positionX = (distanceX * (numberOfColumns - 1)) + 100;
+            } else {
+                rowType = "even";
+                positionX = 100;
+            }
+        } else {
+            if (rowType === "even") {
+                positionX = positionX + distanceX;
+            } else {
+                positionX = positionX - distanceX;
+            }
+        }
     }
 }
-
-
 
